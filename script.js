f218bdbf3fa5883ccfb2e956da528fc8ed185460
Isabel Neves <nevesisa30@gmail.com>
2026-02-03 14:54:23 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,12 @@
+const whatsappBtn = document.getElementById('whatsappBtn');
+
+// ⚠️ Coloque seu número aqui (DDD + número, sem espaços e sem símbolos)
+const phoneNumber = '5511990035200';
+
+// Mensagem automática
+const message = 'Olá! Gostaria de solicitar um orçamento para um site.';
+
+whatsappBtn.addEventListener('click', function () {
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  window.open(whatsappURL, '_blank');
+});
